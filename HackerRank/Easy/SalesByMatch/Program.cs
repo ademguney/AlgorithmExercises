d71524c11d808140177652642daae22fdf6777d2
Adem Güney (Zer A.Ş.) <EX_ADEMG@koczer.com>
2022-05-07 19:28:35 +0300

    Hackerrank, SalesByMatch, Easy

---
+++ HackerRank/Easy/SalesByMatch/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = 9;
+List<int> ar = new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+int result;
+
+static int SockMerchant(int n, List<int> ar)
+{
+    int pairs=0;
+    var socksSet=new HashSet<int>();
+
+
+    foreach (var item in ar)
+    {
+        if (!socksSet.Contains(item))
+        {
+            socksSet.Add(item);
+        }
+        else
+        {
+            pairs++;
+            socksSet.Remove(item);
+        }
+    }  
+    return pairs;
+   
+}
+
+
+result=SockMerchant(n, ar);
+Console.WriteLine("Pair Socks: {0}",result);
+Console.ReadLine();
