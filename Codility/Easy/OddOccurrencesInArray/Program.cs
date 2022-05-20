a9a3dccc7e0fd0777103e56f650454a8d3580c71
Adem Güney (Zer A.Ş.) <EX_ADEMG@koczer.com>
2022-05-20 22:39:10 +0300

    Codility, OddOccurrencesInArray, Easy

---
+++ Codility/Easy/OddOccurrencesInArray/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] A = { 9, 3, 9, 3, 9, 7, 9 };
+
+int solution(int[] A)
+{
+    var set=new HashSet<int>();
+    int result = 0;
+    for (int i=0; i<A.Length; i++)
+    {
+        if (!set.Contains(A[i]))
+        {
+            set.Add(A[i]);
+            result=A[i];
+        }
+        else
+        {
+            set.Remove(A[i]);
+        }
+    }
+    return result;
+}
+
+solution(A);
