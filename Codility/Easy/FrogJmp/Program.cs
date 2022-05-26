399244374c644891d9a63fae93ac50db8bf488da
Adem Güney (Zer A.Ş.) <EX_ADEMG@koczer.com>
2022-05-26 20:55:39 +0300

    Codility, FrogJmp, Easy

---
+++ Codility/Easy/FrogJmp/Program.cs
@@ -0,0 +1,15 @@
+﻿int X = 10, Y = 85, D = 30;
+
+int solution(int X, int Y, int D)
+{
+    int jumpCount = 0;
+    while (X<=Y)
+    {
+        X = X + D;
+        jumpCount++;
+    }
+
+    return jumpCount;
+}
+
+solution(X, Y, D);  
